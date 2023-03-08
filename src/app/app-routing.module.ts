2b97fc5de0a4b429cd5649ea398088a5760e9769
Azguegou <axel.brun@etu.u-paris.fr>
2023-03-08 10:58:23 +0100

    structure site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormulaireComponent } from './formulaire/formulaire.component';
+import { ResultatsComponent } from './resultats/resultats.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'formulaire', component: FormulaireComponent },
+  { path: 'resultats', component: ResultatsComponent },
+  { path: '', component: AppComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

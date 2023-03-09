a8aea4c3d34ecfce15f0950723468b2385e45f62
ickx <mizoudaaa@gmail.com>
2023-03-09 14:55:51 +0100

    field adresse mail + vérif format

--- src/app/formulaire/formulaire.component.ts
+++ src/app/formulaire/formulaire.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-formulaire',
@@ -6,6 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./formulaire.component.css']
 })
 export class FormulaireComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
 
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Adresse mail invalide' : '';
+  }
 }
  

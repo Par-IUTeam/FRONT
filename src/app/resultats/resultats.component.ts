ff2585d666947205fc632f51b83f02f1ce86658e
Azguegou <axel.brun@etu.u-paris.fr>
2023-03-09 15:00:29 +0100

    ajout du tableau et sort dans resultat

--- src/app/resultats/resultats.component.ts
+++ src/app/resultats/resultats.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlimentModel } from '../z_modeles/aliment-classement.model';
 
 @Component({
   selector: 'app-resultats',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./resultats.component.css']
 })
 export class ResultatsComponent {
+  nomsAliments: string[] = ["patate", "orange", "faux-filet", "pâtes", "pomme", "côte de veau", "riz", "banane", "steak", "farine", "abricot", "cuisse de poulet"];
+  categoriesAliments: string[] = ["féculent", "fruit", "viande"];
+  listAlimentsClassement: AlimentModel[] = [];
 
+  constructor(){
+    for (let i = 0; i < 100; ++i){
+      this.listAlimentsClassement.push(new AlimentModel(i, this.nomsAliments[i % 12], this.categoriesAliments[i % 3], (i+8)*100));
+    }
+    this.listAlimentsClassement.sort((a,b) => b.nbSelections - a.nbSelections);
+  }
 }

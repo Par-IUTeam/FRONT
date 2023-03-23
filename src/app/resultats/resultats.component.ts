17410aba9068ca854b941b0be29c682b6850cada
Azguegou <axel.brun@etu.u-paris.fr>
2023-03-23 15:13:24 +0100

    plein de trucs cools

--- src/app/resultats/resultats.component.ts
+++ src/app/resultats/resultats.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AlimentModel } from '../z_modeles/aliment-classement.model';
+import { Aliment } from '../z_modeles/aliment-classement.model';
 
 @Component({
   selector: 'app-resultats',
@@ -7,14 +7,7 @@ import { AlimentModel } from '../z_modeles/aliment-classement.model';
   styleUrls: ['./resultats.component.css']
 })
 export class ResultatsComponent {
-  nomsAliments: string[] = ["patate", "orange", "faux-filet", "pâtes", "pomme", "côte de veau", "riz", "banane", "steak", "farine", "abricot", "cuisse de poulet"];
-  categoriesAliments: string[] = ["féculent", "fruit", "viande"];
-  listAlimentsClassement: AlimentModel[] = [];
+  listAlimentsClassement: Aliment[] = [];
 
-  constructor(){
-    for (let i = 0; i < 100; ++i){
-      this.listAlimentsClassement.push(new AlimentModel(i, this.nomsAliments[i % 12], this.categoriesAliments[i % 3], (i+8)*100));
-    }
-    this.listAlimentsClassement.sort((a,b) => b.nbSelections - a.nbSelections);
-  }
+  constructor(){}
 }
